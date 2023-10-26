65da21fabc02047dffac70ab36ed99b2470cdbb8
Christian Martinez <dantedeat@hotmail.com>
2023-10-25 20:05:06 -0600

    25-oct-23

--- src/components/first-navbar/FirstNavBar.jsx
+++ src/components/first-navbar/FirstNavBar.jsx
@@ -5,20 +5,21 @@ const FirstNavBar = () => {
   return (
     <>
     <div className='first-navbar'>
-
-      <section className='section-left'>
-        <RoomIcon className='ub-icon'/>
-        <p className='adress'>Matamoros #333 Col. Centro | correo@example.com</p>
-      </section>
-      
-      <ul >
+     
+      <p className='adress-email'><RoomIcon className='locatio-icon'/>    Matamoros #333 Col. Centro ||  correo@example.com</p>
+    
+    <div>
+      <ul>
         <li>Contactanos</li>
-        <li>Nosotros</li>
-        <li>Productos</li>
+        <li>Sobre Nosotros</li>
+        <li>Sobre nuestros productos</li>
       </ul>
-      </div>
+    </div>
+      
+   </div>
     </>
   )
 }
 
 export default FirstNavBar
+
