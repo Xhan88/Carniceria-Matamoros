8bcea387076648d09aa5a1710b6cfb62ce4beacd
Christian Martinez <dantedeat@hotmail.com>
2023-12-08 22:55:02 -0600

    first-navbar

--- src/components/first-navbar/FirstNavBar.jsx
+++ src/components/first-navbar/FirstNavBar.jsx
@@ -1,25 +1,25 @@
 import RoomIcon from '@mui/icons-material/Room';
-import '../first-navbar/first-navbar.css'
+import '../first-navbar/first-navbar.css';
 
 const FirstNavBar = () => {
   return (
     <>
-    <div className='first-navbar'>
-     
-      <p className='adress-email'><RoomIcon className='locatio-icon'/>    Matamoros #333 Col. Centro ||  correo@example.com</p>
-    
-    <div>
-      <ul>
-        <li>Contactanos </li>  s
-        <li>Nosotros </li> 
-        <li>Productos</li>
-      </ul>
-    </div>
-      
-   </div>
+      <div className='first-navbar'>
+        <p className='adress-email'>
+          <RoomIcon className='locatio-icon' />
+          Matamoros #333 Col. Centro || correo@example.com
+        </p>
+
+        <div>
+          <ul>
+            <li>Contactanos </li>
+            <li>Nosotros </li>
+            <li>Productos</li>
+          </ul>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default FirstNavBar
-
+export default FirstNavBar;
