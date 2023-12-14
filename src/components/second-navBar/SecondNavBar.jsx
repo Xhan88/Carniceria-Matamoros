9076425034e39b94b9f966b5b2caaaa4c270ac6c
Christian Martinez <dantedeat@hotmail.com>
2023-12-13 22:02:37 -0600

    13-dic-23

--- src/components/second-navBar/SecondNavBar.jsx
+++ src/components/second-navBar/SecondNavBar.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import '../second-navBar/secondNavBar.css'
+
+import React from 'react';
+import '../second-navBar/secondNavBar.css';
 
 const SecondNavBar = () => {
   return (
     <div>
+      
       <nav className="navbar">
         <div className="navbar-container">
           <img className='car-logo' src="./carn-logo.png" alt="" />
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li className='li-hover'>Home</li>
+            <li className='li-hover'>About</li>
+            <li className='li-hover'>Contact</li>
           </ul>
         </div>
       </nav>
     </div>
   );
-
 }
 
-export default SecondNavBar
+export default SecondNavBar;
