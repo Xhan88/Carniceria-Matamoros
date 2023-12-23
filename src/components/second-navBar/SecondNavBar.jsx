606e16233a8f47452c2ecda5b2126e49c34cfe59
Christian Martinez <dantedeat@hotmail.com>
2023-12-22 21:48:24 -0600

    22-12-23

--- src/components/second-navBar/SecondNavBar.jsx
+++ src/components/second-navBar/SecondNavBar.jsx
@@ -12,6 +12,7 @@ const SecondNavBar = () => {
           <ul>
             <li className='li-hover'>Home</li>
             <li className='li-hover'>About</li>
+            <li className='li-hover'>Products</li>
             <li className='li-hover'>Contact</li>
           </ul>
           <h3 className='phone-number'>378 78 2 0347</h3>
