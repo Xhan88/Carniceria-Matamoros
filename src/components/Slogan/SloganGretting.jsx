583b448c5db31541ed2d9d889c8a2e138792c0d6
Christian Martinez <dantedeat@hotmail.com>
2023-12-25 23:58:30 -0600

    24-12-2023

--- src/components/Slogan/SloganGretting.jsx
+++ src/components/Slogan/SloganGretting.jsx
@@ -2,8 +2,10 @@ import './SloganGretting.css'
 const SloganGretting = () => {
   return (
     <>
-    <div>
-      <h1 className='gretting'>Carne Fresca</h1>
+    <div className="gretting-presentation animate__animated  animate__slideInUp">
+      <h1 className='gretting' >Carne Fresca</h1>
+      <p className='gretting'>Carne Suprema</p>
+      <buton className='btn-order'>Ordene Ahora</buton>
     </div>
       
     </>
