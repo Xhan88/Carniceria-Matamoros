8d489e2e02ba046e6aa7012dd039acae40c27131
Christian Martinez <dantedeat@hotmail.com>
2023-12-26 19:18:40 -0600

    26-12-2023

--- src/components/Slogan/SloganGretting.jsx
+++ src/components/Slogan/SloganGretting.jsx
@@ -1,11 +1,14 @@
+import PresentationImages from "../images-secction-gretting/PresentationImages"
 import './SloganGretting.css'
+
 const SloganGretting = () => {
   return (
     <>
     <div className="gretting-presentation animate__animated  animate__slideInUp">
       <h1 className='gretting' >Carne Fresca</h1>
       <p className='gretting'>Carne Suprema</p>
-      <buton className='btn-order'>Ordene Ahora</buton>
+      <button className='btn-order'>Ordene Ahora</button>
+      <PresentationImages />
     </div>
       
     </>
---
+++ src/components/images-secction-gretting/PresentationImages.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../images-secction-gretting/PresentationImages.css'
+
+const PresentationImages = () => {
+  return (
+    <>
+     <div>
+     <img className='img-rib' src="../ribeye.png" alt="" />
+    </div>
+    </>
+   
+  )
+}
+
+export default PresentationImages
