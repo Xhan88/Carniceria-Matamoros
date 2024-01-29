c14a5ce652ce369947f3e2feeaaa6d87ab350bf0
Christian Martinez <dantedeat@hotmail.com>
2024-01-28 21:08:06 -0600

    28-01-24

--- src/components/offer-secction/ProductsOffer.jsx
+++ src/components/offer-secction/ProductsOffer.jsx
@@ -4,25 +4,25 @@ const ProductsOffer = () => {
   return (
     <>
     <div className='container'>
-      <div className='offerProducts1'>
+      <div className='offerProducts'>
         <h3>Productos Frezcos</h3>
         <h2>Lorem, ipsum dolor.</h2>
         <p>Lorem ipsum dolor sit amet consectetur.</p>
         <h1>$ 176</h1>
     <div>
-      <img src="/meat-card1.png" alt="" />
+      <img className="card-images" src="/meat-card1.png" alt="" />
     </div>
         
       </div>
 
-      <div className='offerProducts2'>
+      <div className='offerProducts'>
         <h3>Productos Frezcos</h3>
         <h2>Lorem, ipsum dolor.</h2>
         <p>Lorem ipsum dolor sit amet consectetur.</p>
-        <h1>$ 176</h1>
+        <h1>$ 90</h1>
 
         <div>
-          <img src="/meat-card2.png" alt="" />
+          <img className="card-images" src="/meat-card2.png" alt="" />
         </div>
 
       </div>
