5e44a95814607a1bd512cb3a1c4556fd83269c0c
Christian Martinez <dantedeat@hotmail.com>
2024-01-30 22:16:43 -0600

    30-01-24

--- src/components/organic-products/OrganicProducts.jsx
+++ src/components/organic-products/OrganicProducts.jsx
@@ -1,12 +1,22 @@
 import './OrganicProducts.css'
+import OrganicCards from './OrganicCards'
+
 
 const OrganicProducts = () => {
   return (
+    <>
     <div  className="secction-organic">
       <p>Nuestros productos organicos</p>
       <h1>Organic Products</h1>
       <img src="../shape.png" alt="" />
+       
     </div>
+<div className='Organic-cards'>
+          
+          <OrganicCards />
+          
+      </div>
+      </>
   )
 }
 
--- src/components/organic-products/organicCards.jsx
+++ src/components/organic-products/organicCards.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
 
-const organicCards = () => {
+const OrganicCards = () => {
   return (
     <>
     <div>
-
+      <img src="meat-card1.png" alt="" />
+      <h4>Hamburguer meat</h4>
+      <p>$ 176</p>
+      <p>carne suprema</p>
     </div>
     </>
   )
 }
 
-export default organicCards
+export default OrganicCards
