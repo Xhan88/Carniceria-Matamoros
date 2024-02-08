a1cb65ca8b9ab4dcf647decbd5431bc71305ec45
Christian Martinez <dantedeat@hotmail.com>
2024-02-08 07:03:55 -0600

    08-02-24

--- src/components/organic-products/organicCards.jsx
+++ src/components/organic-products/organicCards.jsx
@@ -3,8 +3,9 @@ import '../organic-products/OrganicCard.css'
 const OrganicCards = () => {
   return (
     <>
+    <img className="img-organic-card" src="meat-card1.png" alt="" />
     <div>
-      <img className="img-organic-card" src="meat-card1.png" alt="" />
+      
       <h4>Hamburguer meat</h4>
       <p>$ 176</p>
       <p>carne suprema</p>
