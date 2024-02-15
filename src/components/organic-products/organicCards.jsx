4331068ac7e6367624d75697724e66fef690230e
Christian Martinez <dantedeat@hotmail.com>
2024-02-14 23:04:02 -0600

    14-02-24

--- src/components/organic-products/organicCards.jsx
+++ src/components/organic-products/organicCards.jsx
@@ -3,27 +3,31 @@ import '../organic-products/OrganicCard.css'
 const OrganicCards = () => {
   return (
     <>
-    <img className="img-organic-card" src="meat-card1.png" alt="" />
-    <div>
-      
-      <h4>Hamburguer meat</h4>
-      <p>$ 176</p>
-      <p>carne suprema</p>
-    </div>
+    
     <div>
       <img className="img-organic-card" src="meat-card1.png" alt="" />
       <h4>Hamburguer meat</h4>
       <p>$ 176</p>
-      <p>carne suprema</p>
+      <p className='textCard'>carne suprema</p>
     </div>
+
     <div>
-      <img className="img-organic-card" src="meat-card1.png" alt="" />
+      <img className="img-organic-card" src="meat-card2.png" alt="" />
       <h4>Hamburguer meat</h4>
       <p>$ 176</p>
-      <p>carne suprema</p>
+      <p className='textCard'>carne suprema</p>
     </div>
+
+    <div>
+      <img className="img-organic-card" src="meat3.png" alt="" />
+      <h4>Hamburguer meat</h4>
+      <p>$ 186</p>
+      <p className='textCard'>carne suprema</p>
+    </div>
+
     </>
   )
+  
 }
 
 export default OrganicCards
