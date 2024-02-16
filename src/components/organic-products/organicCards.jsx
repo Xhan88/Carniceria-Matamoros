daef44680f222b5f370706def28cec139c0cc895
Christian Martinez <dantedeat@hotmail.com>
2024-02-15 23:33:11 -0600

    15-02-24

--- src/components/organic-products/organicCards.jsx
+++ src/components/organic-products/organicCards.jsx
@@ -4,25 +4,25 @@ const OrganicCards = () => {
   return (
     <>
     
-    <div>
+    <div className='card-meat'>
       <img className="img-organic-card" src="meat-card1.png" alt="" />
       <h4>Hamburguer meat</h4>
       <p>$ 176</p>
-      <p className='textCard'>carne suprema</p>
+      <p className='textCardFooter'>carne suprema</p>
     </div>
 
-    <div>
+    <div className='card-meat'>
       <img className="img-organic-card" src="meat-card2.png" alt="" />
       <h4>Hamburguer meat</h4>
       <p>$ 176</p>
-      <p className='textCard'>carne suprema</p>
+      <p className='textCardFooter'>carne suprema</p>
     </div>
 
-    <div>
+    <div className='card-meat'>
       <img className="img-organic-card" src="meat3.png" alt="" />
       <h4>Hamburguer meat</h4>
       <p>$ 186</p>
-      <p className='textCard'>carne suprema</p>
+      <p className='textCardFooter'>carne suprema</p>
     </div>
 
     </>
