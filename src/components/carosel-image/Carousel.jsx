958b7620a420857c0eca19aab68f6af9308ddd99
Christian Martinez <dantedeat@hotmail.com>
2024-03-08 21:00:08 -0600

    08-03-24

--- src/components/carosel-image/Carousel.jsx
+++ src/components/carosel-image/Carousel.jsx
@@ -14,22 +14,22 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
+    
     imgPath:
       'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/eiliv-aceron-AQ_BdsvLgqA-unsplash.jpg?alt=media&token=2f3d4cd1-9aac-499c-b6f8-abf1084e2856',
   },
   {
-    label: 'Bird',
+    
     imgPath:
       'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/charlie-solorzano-utTJUcvNXXo-unsplash.jpg?alt=media&token=61bbc301-8e99-418a-8e9e-78e983c410b3',
   },
   {
-    label: 'Bali, Indonesia',
+    
     imgPath:
       'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/kyle-mackie-QH8SHBARVVk-unsplash.jpg?alt=media&token=c00820d6-2170-4364-adce-84792c101e66',
   },
   {
-    label: 'Goč, Serbia',
+    
     imgPath:
       'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/madie-hamilton-GXehL5_crJ4-unsplash.jpg?alt=media&token=55fa3d3b-01b4-42c7-b25a-1b02a8b6eb0f',
   },
@@ -60,7 +60,6 @@ function SwipeableTextMobileStepper() {
         sx={{
           display: 'flex',
           alignItems: 'center',
-          height: 100,
           pl: 1,
           bgcolor: 'background.default',
         }}
@@ -80,6 +79,7 @@ function SwipeableTextMobileStepper() {
                 component="img"
                 sx={{
                   height: 900,
+                  alignItems: 'center',
                   display: 'block',
                   maxWidth: 1500,
                   overflow: 'hidden',
