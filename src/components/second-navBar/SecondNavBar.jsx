b2279ed2d218876762c9e93ca77bdc6bab646963
Christian Martinez <dantedeat@hotmail.com>
2024-03-09 20:47:40 -0600

    09-03-24

--- src/components/second-navBar/SecondNavBar.jsx
+++ src/components/second-navBar/SecondNavBar.jsx
@@ -1,31 +1,4 @@
 
-// import React from 'react';
-// import '../second-navBar/secondNavBar.css';
-
-// const SecondNavBar = () => {
-//   return (
-//     <div>
-      
-//       <nav className="navbar">
-//         <div className="navbar-container">
-//           <img className='car-logo' src="./carn-logo.png" alt="" />
-//           <ul>
-//             <li className='li-hover'>Home</li>
-//             <li className='li-hover'>About</li>
-//             <li className='li-hover'>Products</li>
-//             <li className='li-hover'>Contact</li>
-//           </ul>
-//           <h3 className='phone-number'>378 78 2 0347</h3>
-//         </div>
-       
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default SecondNavBar;
-
-
 import React, { useEffect, useState } from 'react';
 import '../second-navBar/secondNavBar.css';
 
