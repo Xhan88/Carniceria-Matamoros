e74f380453559a6546ff5672c16ad573057fdbb9
Christian Martinez <dantedeat@hotmail.com>
2024-03-11 22:30:55 -0600

    correctio-error

--- src/components/second-navBar/SecondNavBar.jsx
+++ src/components/second-navBar/SecondNavBar.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-import './secondNavBar.css';
+import './SecondNavBar.css';
 
 const SecondNavBar = () => {
   const [isSticky, setIsSticky] = useState(false);
