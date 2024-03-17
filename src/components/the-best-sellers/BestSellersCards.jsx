9e3dd1259defe79db115ae60d9f2587042e83401
Christian Martinez <dantedeat@hotmail.com>
2024-03-16 22:56:10 -0600

    16-03-24

--- src/components/the-best-sellers/BestSellersCards.jsx
+++ src/components/the-best-sellers/BestSellersCards.jsx
@@ -4,15 +4,15 @@ const BestSellersCards = (props) => {
   const {title, description, price, image } = props;
   return (
     <>
-    <div className= 'container-sellers'>
-    <div className='content-seller' >
-        <img className="image-bestseller" src={image} alt={title} />
-        <p> {title}</p>
-        <p>{description}</p>
-        <h3>{price}</h3>
-      </div>
-      
-      </div>
+    <div className='best-seller-cont'>
+      <div className='content-seller'>
+
+            <img className="image-bestseller" src={image} alt={title} />
+              <p> {title}</p>
+              <p>{description}</p>
+            <h3>{price}</h3>
+          </div>
+</div>
     </>
   )
 }
