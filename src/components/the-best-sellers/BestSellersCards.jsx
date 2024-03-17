8b8522b352b9d5c6984a999bcb57f85f941bf3f0
Christian Martinez <dantedeat@hotmail.com>
2024-03-17 16:44:45 -0600

    17-03-24

--- src/components/the-best-sellers/BestSellersCards.jsx
+++ src/components/the-best-sellers/BestSellersCards.jsx
@@ -1,18 +1,18 @@
-import './BestSellerCards.css'
+import './BestSellerCards.css';
 
 const BestSellersCards = (props) => {
-  const {title, description, price, image } = props;
+  const { title, description, price, image } = props;
   return (
     <>
-    <div className='best-seller-cont'>
-      <div className='content-seller'>
+      <div className='best-seller-cont'>
+        <div className='content-seller'>
 
-            <img className="image-bestseller" src={image} alt={title} />
-              <p> {title}</p>
-              <p>{description}</p>
-            <h3>{price}</h3>
-          </div>
-</div>
+          <img className="image-bestseller" src={image} alt={title} />
+          <p>{title}</p>
+          <p>{description}</p>
+          <h3>{price}</h3>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/the-best-sellers/CardSellers.jsx
+++ src/components/the-best-sellers/CardSellers.jsx
@@ -8,32 +8,32 @@ const CardSellers = () => {
     {
       title: 'Rib-Eye',
       description: 'Description for Card 1',
-      image: 'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/rib-eye.jpg?alt=media&token=7c93f6f0-7eb8-42fc-b41d-0202f49fa10f',
+      image: 'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/rib-eye-removebg-preview%20(1).png?alt=media&token=9ce81b20-ba82-428d-81c5-17ac8ecb0d62',
       price: 200,
     },
     {
       title: 'Tex-Mex',
       description: 'Description for Card 2',
-      image: 'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/tex-mex.jpeg?alt=media&token=3f806ee0-d91e-4ab2-acf6-d8086067fca9',
+      image: 'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/tex-mex-removebg-preview.png?alt=media&token=b5915407-adce-425e-b81c-17367cf991c3',
       price: 200,
     },
     {
       title: 'Filete de Res',
       description: 'Description for Card 3',
-      image: 'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/filete-res.jpeg?alt=media&token=3c39f518-cf80-4b4c-a918-f0c355c9d7e1',
+      image: 'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/filete-res-removebg-preview.png?alt=media&token=912a65d4-b911-4c1d-a5f6-528582ad039c',
       price: 200,
     },
     {
       title: 'Diezmillo',
       description: 'Description for Card 4',
-      image: 'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/diezmillo.jpeg?alt=media&token=2c5bb9f8-e4db-4a55-bb95-8b1b26bbf81d',
+      image: 'https://firebasestorage.googleapis.com/v0/b/carniceria-matamoros.appspot.com/o/diezmillo-removebg-preview.png?alt=media&token=4835d339-5916-417c-8d1e-cb00388a3211',
       price: 200,
     },
   ];
   
   return (
     
-    <div>
+    <div className="card-component">
       {cardData.map((card, index) => (
         <BestSellersCards key={index} {...card} />
       ))}
