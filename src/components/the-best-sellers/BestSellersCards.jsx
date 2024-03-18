dd2a0b0cadb87a83762dc01e13e6944e540d6061
Christian Martinez <dantedeat@hotmail.com>
2024-03-17 23:03:16 -0600

    17-03-24

--- src/components/the-best-sellers/BestSellersCards.jsx
+++ src/components/the-best-sellers/BestSellersCards.jsx
@@ -8,9 +8,12 @@ const BestSellersCards = (props) => {
         <div className='content-seller'>
 
           <img className="image-bestseller" src={image} alt={title} />
-          <p>{title}</p>
-          <p>{description}</p>
-          <h3>{price}</h3>
+          <section className='text-p'>
+             <p className='text-title'>{title}</p>
+             <p className='text-description'>{description}</p>
+          </section>
+         
+          <h3>${price}</h3>
         </div>
       </div>
     </>
