4594eab0ab0d3a61bbb97410a98c5ca21e977c40
Christian Martinez <dantedeat@hotmail.com>
2024-03-21 22:20:03 -0600

    21-03-24

--- src/components/Our-Team/OurTeam.jsx
+++ src/components/Our-Team/OurTeam.jsx
@@ -3,7 +3,7 @@ import PersonalCards from './PersonalCards'
 const OurTeam = () => {
   return (
     <div className='our-team'>
-      <h4>Reunete con el equipo</h4>
+      <h3>Reunete con el equipo</h3>
       <h2>Nuestro CREATIVO equipo</h2>
       <h4>Compra los mejores productos, trabajado por manos profecionales</h4>
 
