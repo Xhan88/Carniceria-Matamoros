fd7bb1e2acad8535736885f42d02cdb7f65aa7ec
Christian Martinez <dantedeat@hotmail.com>
2024-10-22 20:49:10 -0600

    22 de oct 2024

--- src/components/second-navBar/SecondNavBar.jsx
+++ src/components/second-navBar/SecondNavBar.jsx
@@ -1,46 +1,50 @@
-
 import React, { useEffect, useState } from 'react';
 import './SecondNavBar.css';
 
 const SecondNavBar = () => {
   const [isSticky, setIsSticky] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false); // Nuevo estado para controlar el menú de hamburguesa
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth' // Para un desplazamiento suave
+      behavior: 'smooth',
     });
   };
-  
-  // <button onClick={scrollToTop}>Volver arriba</button>
 
   useEffect(() => {
     const handleScroll = () => {
       const offset = window.scrollY;
-
-      
       setIsSticky(offset > 50);
     };
 
-    
     window.addEventListener('scroll', handleScroll);
-
-    
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Abrir o cerrar el menú al hacer clic en el botón de hamburguesa
+  };
+
   return (
     <div>
       <nav className={`navbar ${isSticky ? 'sticky' : ''}`}>
         <div className="navbar-container">
-          <img onClick={scrollToTop} className='car-logo' src="./carn-logo.png" alt="" />
-          <ul>
+          <img onClick={scrollToTop} className='car-logo' src="./carn-logo.png" alt="Logo" />
+          
+          <div className="menu-toggle" onClick={toggleMenu}>
+            &#9776; {/* Icono de hamburguesa */}
+          </div>
+
+          <ul className={`navbar-menu ${menuOpen ? 'open' : ''}`}>
             <li className='li-hover'>Home</li>
             <li className='li-hover'>About</li>
             <li className='li-hover'>Products</li>
             <li className='li-hover'>Contact</li>
           </ul>
+          
           <h3 className='phone-number'>378 78 2 0347</h3>
         </div>
       </nav>
